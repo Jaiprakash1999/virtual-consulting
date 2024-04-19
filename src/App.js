d7af9420a00abcb689f814ee580de7ec46c2517e
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2024-04-19 17:34:50 +0530

    comment remove

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import PatientRegister from "./patient/patient-register/PatientRegister";
 import PatientLogin from "./patient/patient-login/PatientLogin";
 import DoctorRegister from "./doctor/doctor-register/DoctorRegister";
-import VirtualConsultingImage from "../src/images/virtualConsulting.jpg";
 import DoctorLogin from "./doctor/doctor-login/DoctorLogin";
 
 function App() {
@@ -15,8 +14,6 @@ function App() {
       <div className="flex justify-center">
         <img
           src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fd3d6901b3eee8fef03e9158a6c7e1ca6.cdn.bubble.io%2Ff1689764109471x430181511902805400%2FImage%252019-07-23%2520at%25204.20%2520PM.jpg?w=96&h=82&auto=compress&dpr=2&fit=max"
-          // src={VirtualConsultingImage}
-          // src="https://www.practostatic.com/consumer-home/mweb/images/1597391270/instant_video_consult.png"
           alt="virtualImage"
         />
       </div>
